b73d668422878dfad017f696001f5bfe58a594de
DUA-UMER <161318323+DUA-UMER@users.noreply.github.com>
2024-04-27 23:50:10 +0500

    Add files via upload

---
+++ Que06 Stripping name/app.js
@@ -0,0 +1,4 @@
+var whitespaceName = "\n\t DUA-UMER \t\n";
+console.log(whitespaceName);
+var withoutwhitespaceName = whitespaceName.trim();
+console.log(withoutwhitespaceName);
---
+++ Que06 Stripping name/app.ts
@@ -0,0 +1,5 @@
+let whitespaceName = "\n\t DUA-UMER \t\n";
+console.log(whitespaceName);
+
+let withoutwhitespaceName = whitespaceName.trim();
+console.log(withoutwhitespaceName);
