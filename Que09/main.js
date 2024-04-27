8915f3fc03f037f5f9e4416e877b2eda0848c8a2
DUA-UMER <161318323+DUA-UMER@users.noreply.github.com>
2024-04-27 23:59:17 +0500

    Add files via upload

---
+++ Que09/main.js
@@ -0,0 +1,3 @@
+var favNumber = 31;
+var message = "mine favorit number is:";
+console.log(message, favNumber);
---
+++ Que09/main.ts
@@ -0,0 +1,3 @@
+let favNumber = 31;
+let message = "mine favorit number is:";
+console.log(message , favNumber);
