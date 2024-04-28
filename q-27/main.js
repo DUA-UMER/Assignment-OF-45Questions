725d7a6222853a469e2d789580e8edd6c5c8f4e0
DUA-UMER <161318323+DUA-UMER@users.noreply.github.com>
2024-04-28 16:10:46 +0500

    Add files via upload

---
+++ q-27/main.js
@@ -0,0 +1,32 @@
+var alienColour = " green";
+if (alienColour === " green") {
+    console.log("you shot down green alien so you have earned 5 pints.");
+}
+else if (alienColour === "yellow") {
+    console.log("you shot down yellow alien so you have earned 10 points.");
+}
+else if (alienColour === "red") {
+    console.log("you shot down red alien so you hav earned 15 points.");
+}
+//version 2 
+var alienColour2 = "yellow";
+if (alienColour2 === "green") {
+    console.log("you shot down green alien so you have earned 5 points.");
+}
+else if (alienColour2 === "yellow") {
+    console.log("you shot down yellow alien so you have earned  10 points");
+}
+else if (alienColour2 === "red") {
+    console.log("you shot down red alien so you have earned 15 points.");
+}
+//version 3
+var alienColour3 = "red";
+if (alienColour3 === "geen") {
+    console.log("you shot down green alien so you have earned 5 points.");
+}
+else if (alienColour3 === "red") {
+    console.log("you shot down red alien so you have earned 15 points.");
+}
+else if (alienColour3 === "yellow") {
+    console.log("you shot down yellow alien so you have earned 10 points.");
+}
