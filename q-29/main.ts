4af9a77d7c31e4a3234df6dc49cc972c486c1bae
DUA-UMER <161318323+DUA-UMER@users.noreply.github.com>
2024-04-28 16:12:35 +0500

    Add files via upload

---
+++ q-29/main.ts
@@ -0,0 +1,17 @@
+let favFruits = ["mango","strawberry","orange"];
+
+if(favFruits.includes("mango")){
+console.log("i really like mango")
+}
+if(favFruits.includes("strawberry")){
+    console.log("i really like strawberry")
+}
+if (favFruits.includes("banana")){
+  console.log("i really like banana")
+}
+if (favFruits.includes("orange")){
+    console.log("i really like orange")
+}
+if (favFruits.includes("apple")){
+    console.log("i really like apple")
+}
