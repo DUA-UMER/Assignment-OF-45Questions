f0dc2492525b0cd5501ee6c1b901a4c17f417e77
DUA-UMER <161318323+DUA-UMER@users.noreply.github.com>
2024-04-28 16:18:16 +0500

    Add files via upload

---
+++ q-36/main.js
@@ -0,0 +1,4 @@
+function makeShirt(size, printMessage) {
+    console.log("you selected ".concat(size, " size shirt with ").concat(printMessage, " print on shirt"));
+}
+makeShirt("Medium", "i am best");
---
+++ q-36/main.ts
@@ -0,0 +1,5 @@
+function makeShirt(size: string, printMessage : string){
+    console.log(`you selected ${size} size shirt with ${printMessage} print on shirt`)
+}
+
+makeShirt("Medium","i am best")
