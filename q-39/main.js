eefef743cf3a6b84e1e682ed8ed073240bbdc057
DUA-UMER <161318323+DUA-UMER@users.noreply.github.com>
2024-04-28 16:23:26 +0500

    Add files via upload

---
+++ q-39/main.js
@@ -0,0 +1,10 @@
+function cityCountry(city, country) {
+    return "".concat(city, " , ").concat(country);
+}
+//calling a function & print the returned value
+cityCountry("karachi", "pakistan");
+console.log(cityCountry("karachi", "pakistan"));
+cityCountry("tokyo", "japan");
+console.log(cityCountry("tokya", "japan"));
+cityCountry("Berlin", "germany");
+console.log(cityCountry("berlin", "germany"));
---
+++ q-39/main.ts
@@ -0,0 +1,13 @@
+function cityCountry(city: string, country: string) : string{
+     return `${city} , ${country}`;
+}
+
+//calling a function & print the returned value
+ cityCountry("karachi", "pakistan");
+ console.log(cityCountry("karachi", "pakistan"));
+
+ cityCountry("tokyo", "japan");
+ console.log(cityCountry("tokya", "japan"));
+
+ cityCountry("Berlin", "germany");
+ console.log(cityCountry("berlin", "germany"));
