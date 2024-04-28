4d4825e2786b7b52fce8f06dd588c83e2e642fb2
DUA-UMER <161318323+DUA-UMER@users.noreply.github.com>
2024-04-28 15:47:12 +0500

    Add files via upload

---
+++ q-19/main.js
@@ -0,0 +1,5 @@
+"use strict";
+let guestList = ["DUA", "UMER", "ABRISH", "AMNA", "sana"];
+//guestList.forEach(guest => console.log(`Hi ${guest}, would you like to dinner with me? `));
+let lengthGuests = guestList.length;
+console.log(`we are inviting total ${lengthGuests} guests.`);
---
+++ q-19/main.ts
@@ -0,0 +1,7 @@
+let guestList = ["DUA","UMER","ABRISH","AMNA","sana"];
+
+//guestList.forEach(guest => console.log(`Hi ${guest}, would you like to dinner with me? `));
+
+let lengthGuests : number = guestList.length;
+
+console.log(`we are inviting total ${lengthGuests} guests.`);
