158793968baf81a4ab5e6cf2b14a2d7a886f1e65
DUA-UMER <161318323+DUA-UMER@users.noreply.github.com>
2024-04-28 15:50:42 +0500

    Add files via upload

---
+++ q-20/main.js
@@ -0,0 +1,7 @@
+"use strict";
+//making a programming languages Array 
+let programmingLanguages = ["Typescript", "javascript", "python", "php"];
+//print the message og list
+console.log("List of programming Languages:");
+//ptint the value of Array in the form of List
+programmingLanguages.forEach(Language => console.log(Language));
---
+++ q-20/main.ts
@@ -0,0 +1,8 @@
+//making a programming languages Array 
+let programmingLanguages : string[] = ["Typescript", "javascript", "python","php"];
+
+//print the message og list
+console.log("List of programming Languages:");
+
+//ptint the value of Array in the form of List
+programmingLanguages.forEach(Language => console.log(Language));
