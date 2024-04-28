fa691e74f346b5ac30ff5823e41bfc5fe7993cba
DUA-UMER <161318323+DUA-UMER@users.noreply.github.com>
2024-04-28 15:52:55 +0500

    Add files via upload

---
+++ q-23/main.js
@@ -0,0 +1,33 @@
+//makig a variables
+var five = 5;
+var ten = 10;
+//test 1 
+console.log("test 1 : five is equal to 5");
+console.log(five == 5);
+//test 2
+console.log("\ntest 2 : ten is equal to 10");
+console.log(ten == 10);
+//test 3
+console.log("\ntest 3 : five is not equal to 10");
+console.log(five != 10);
+//test 4
+console.log("\ntest 4 : ten is greater than 5");
+console.log(ten > 5);
+//test 5 
+console.log("\ntest 5 : 5 is smaller than 10");
+console.log(five < 10);
+//test 6
+console.log("\ntest 6 : 10 is smaller than 5");
+console.log(ten < 5);
+//test 7
+console.log("\ntest 7 : 5 is equal to 10");
+console.log(five == 10);
+//test 8 
+console.log("\ntest 8 : 5 is not equal to 5");
+console.log(five != 5);
+//test 9 
+console.log("\ntest 9 : 9 is greater than 10");
+console.log(9 > 10);
+//test 10
+console.log("\ntest 10 : 100 is smaller than 20 ");
+console.log(100 < 20);
