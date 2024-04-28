cfbea12f6ed663f4f8af31c1a289351f4d32d70a
DUA-UMER <161318323+DUA-UMER@users.noreply.github.com>
2024-04-28 15:36:33 +0500

    Add files via upload

---
+++ q-12/main.js
@@ -0,0 +1,2 @@
+var friendsName = ["dua", "umer", "amna", "abrish"];
+friendsName.forEach(function (friendName) { return console.log("Hello ".concat(friendName, ", How are you?")); });
---
+++ q-12/main.ts
@@ -0,0 +1,3 @@
+let friendsName = ["dua","umer","amna","abrish"];
+
+friendsName.forEach(friendName => console.log(`Hello ${friendName}, How are you?`));
