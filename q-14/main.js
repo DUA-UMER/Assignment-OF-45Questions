2a44d0072b07a0509786c3bedb0b7f4d11d341db
DUA-UMER <161318323+DUA-UMER@users.noreply.github.com>
2024-04-28 15:39:29 +0500

    Add files via upload

---
+++ q-14/main.js
@@ -0,0 +1,2 @@
+var guestList = ["DUA", "UMER", "ABRISH", "AMNA"];
+guestList.forEach(function (guest) { return console.log("Hi ".concat(guest, ", would you like to dinner with me? ")); });
---
+++ q-14/main.ts
@@ -0,0 +1,3 @@
+let guestList = ["DUA","UMER","ABRISH","AMNA"];
+
+guestList.forEach(guest => console.log(`Hi ${guest}, would you like to dinner with me? `));
