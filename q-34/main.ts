3f310a497196412b4d764b550db59e4d5caeb1fa
DUA-UMER <161318323+DUA-UMER@users.noreply.github.com>
2024-04-28 16:16:49 +0500

    Add files via upload

---
+++ q-34/main.ts
@@ -0,0 +1,8 @@
+let pizza = ["chicken tikka", "malai cheese","fajita"];
+ //useing for-loop
+ for(let onePizza of pizza){
+   console.log(`I like ${onePizza} pizza`);
+ }
+
+ console.log("pizza is love");
+ console.log("I eat pizza");
