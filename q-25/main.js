f2dbdc95d915e903e2cbb057f4a49ca3a9a8104b
DUA-UMER <161318323+DUA-UMER@users.noreply.github.com>
2024-04-28 15:54:49 +0500

    Add files via upload

---
+++ q-25/main.js
@@ -0,0 +1,9 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+let alienColour = "black";
+if (alienColour === "black") {
+    console.log("alienColour is black player just earned 5 points.");
+}
+if (alienColour === "yellow") {
+    console.log("alienColour is yellow.");
+}
---
+++ q-25/main.ts
@@ -0,0 +1,10 @@
+let alienColour = "black";
+
+if(alienColour === "black"){
+    console.log("alienColour is black player just earned 5 points.");
+}
+
+if(alienColour === "yellow"){
+    console.log("alienColour is yellow.");
+}
+
