c49695e564c049ff68f730d2bfb713b554087e13
DUA-UMER <161318323+DUA-UMER@users.noreply.github.com>
2024-04-28 16:22:52 +0500

    Add files via upload

---
+++ q-38/main.js
@@ -0,0 +1,7 @@
+function describeCity(city, country) {
+    if (country === void 0) { country = "pakistan"; }
+    console.log("".concat(city, " is in ").concat(country));
+}
+//calling the function
+describeCity("karachi");
+describeCity("lahore");
---
+++ q-38/main.ts
@@ -0,0 +1,6 @@
+function describeCity(city: string, country:string = "pakistan"){
+   console.log(`${city} is in ${country}`);
+}
+//calling the function
+describeCity("karachi");
+describeCity("lahore");
