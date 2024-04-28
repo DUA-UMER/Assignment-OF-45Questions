b6e730c436bccda6ca695e33a88c2426ede5e5fb
DUA-UMER <161318323+DUA-UMER@users.noreply.github.com>
2024-04-28 16:05:04 +0500

    Add files via upload

---
+++ q-26/main.js
@@ -0,0 +1,14 @@
+var alienColour = "green";
+if (alienColour === "green") {
+    console.log("player just earned 5 points for shooting this alien");
+}
+else {
+    console.log("player just earned 10 points.");
+}
+//second version
+if (alienColour === "yellow") {
+    console.log("alien is yellow");
+}
+else {
+    console.log("alien is not yellow");
+}
---
+++ q-26/main.ts
@@ -0,0 +1,16 @@
+let alienColour = "green";
+
+if(alienColour === "green"){
+    console.log("player just earned 5 points for shooting this alien");
+
+}
+else{
+    console.log("player just earned 10 points.");
+}
+//second version
+if (alienColour === "yellow"){
+  console.log("alien is yellow")
+}
+else {
+    console.log("alien is not yellow");
+}
